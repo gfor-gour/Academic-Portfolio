4e86c52ba0fefcb066d3ebc05a84b1e81df182f1
unknown <talukdargour.bd@gmail.com>
2026-06-05 16:49:24 +0600

    updating dependencies  for cloudfare deployment

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,7 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({});
+export default {
+  ...defineCloudflareConfig({}),
+  // Run Next directly so `npm run build` can invoke OpenNext without recursion.
+  buildCommand: "next build",
+};
