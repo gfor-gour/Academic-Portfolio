598ee856915212ba22863fa92a552d60fcd2bd2a
unknown <talukdargour.bd@gmail.com>
2026-06-05 16:27:57 +0600

    updating nextjs version for cloudfare deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
@@ -18,3 +20,5 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+initOpenNextCloudflareForDev();
